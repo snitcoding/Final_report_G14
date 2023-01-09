b80af2f9feb233aa5b77f6611b03014893754463
Restyled.io <commits@restyled.io>
2023-01-09 18:27:42 +0000

    Restyled by clang-format

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Get the button
 let mybutton = document.getElementById("myBtn");
-        
+
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
